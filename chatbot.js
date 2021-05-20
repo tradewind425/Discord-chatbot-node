4575359722734933a29e9c1cc1be8333fc6f0be7
とらで <tradewind425@gmail.com>
2021-05-20 16:25:19 +0900

    トークンを公開するなとDiscordに怒られるまでワンセット

--- chatbot.js
+++ chatbot.js
@@ -6,7 +6,7 @@ console.log ("====デバッグ中===");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = "ODQ0ODEwNTM0OTkzMjY0NjUw.YKX1Hw.1zo6hCt4OWZ5DMhM2N-wHi7gQKU";
+const token = "ここにトークン";
 
 client.on("ready",() => {
     console.log(`${client.user.username}としてログインしています。`);
